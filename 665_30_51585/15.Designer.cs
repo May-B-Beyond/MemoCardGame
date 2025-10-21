cd949b1d8ad6971161887f9f3cf34681335f1c01
May-B-Beyond <knamboot@gmail.com>
2025-10-21 18:30:07 +0700

    Game Publish

---
+++ 665_30_51585/15.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace _665_30_51585
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DebugNumber2 = new System.Windows.Forms.Label();
+            this.Debug = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TXTScoreP2 = new System.Windows.Forms.Label();
+            this.TXTScoreP1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(806, 420);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(798, 394);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DebugNumber2);
+            this.tabPage2.Controls.Add(this.Debug);
+            this.tabPage2.Controls.Add(this.button11);
+            this.tabPage2.Controls.Add(this.button10);
+            this.tabPage2.Controls.Add(this.button9);
+            this.tabPage2.Controls.Add(this.button8);
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.TXTScoreP2);
+            this.tabPage2.Controls.Add(this.TXTScoreP1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(798, 394);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DebugNumber2
+            // 
+            this.DebugNumber2.AutoSize = true;
+            this.DebugNumber2.Location = new System.Drawing.Point(347, 65);
+            this.DebugNumber2.Name = "DebugNumber2";
+            this.DebugNumber2.Size = new System.Drawing.Size(35, 13);
+            this.DebugNumber2.TabIndex = 12;
+            this.DebugNumber2.Text = "label1";
+            this.DebugNumber2.Click += new System.EventHandler(this.DebugNumber2_Click);
+            // 
+            // Debug
+            // 
+            this.Debug.AutoSize = true;
+            this.Debug.Location = new System.Drawing.Point(242, 65);
+            this.Debug.Name = "Debug";
+            this.Debug.Size = new System.Drawing.Size(35, 13);
+            this.Debug.TabIndex = 1;
+            this.Debug.Text = "label1";
+            this.Debug.Click += new System.EventHandler(this.Debug_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(422, 231);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(47, 72);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "?";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(125, 231);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(47, 72);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "?";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(422, 115);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(47, 72);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "?";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(125, 115);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(47, 72);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "?";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(350, 231);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(47, 72);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "?";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(350, 115);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(47, 72);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "?";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(277, 231);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(47, 72);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(198, 231);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 72);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(277, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 72);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(47, 72);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TXTScoreP2
+            // 
+            this.TXTScoreP2.AutoSize = true;
+            this.TXTScoreP2.Location = new System.Drawing.Point(8, 16);
+            this.TXTScoreP2.Name = "TXTScoreP2";
+            this.TXTScoreP2.Size = new System.Drawing.Size(237, 13);
+            this.TXTScoreP2.TabIndex = 1;
+            this.TXTScoreP2.Text = "//////////////////////////////////////////////";
+            // 
+            // TXTScoreP1
+            // 
+            this.TXTScoreP1.AutoSize = true;
+            this.TXTScoreP1.Location = new System.Drawing.Point(8, 3);
+            this.TXTScoreP1.Name = "TXTScoreP1";
+            this.TXTScoreP1.Size = new System.Drawing.Size(237, 13);
+            this.TXTScoreP1.TabIndex = 0;
+            this.TXTScoreP1.Text = "//////////////////////////////////////////////";
+            this.TXTScoreP1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TXTScoreP1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label TXTScoreP2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label Debug;
+        private System.Windows.Forms.Label DebugNumber2;
+    }
+}
+
---
+++ 665_30_51585/15.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace _665_30_51585
+{
+    public partial class Form1 : Form
+    {
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Card
+        int ScoreP1 = 0;
+        int ScoreP2 = 0;
+        int NumberP1 = 0;
+        int NumberP2 = 0;
+        int Turn = 1;
+        int State = 0; // ถ้า 0 คือตาที่ 1 ของ Round ถ้า 1 คือ ตาที่ 2 ของRound ( 0 : Self , 1 : Opponent)
+        int CardShowTime = 2000; // ms 
+        public Form1()
+        {   
+            InitializeComponent();
+            
+        }
+        
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            Debug.Text = NumberP1.ToString();
+            DebugNumber2.Text = NumberP2.ToString();
+            TXTScoreP1.Text = "Score P1 >> " + " " + ScoreP1.ToString();
+            TXTScoreP2.Text = "Score P2 >> " + " " + ScoreP2.ToString();
+        }
+
+
+        private void Shuffle(int[] array)
+        {
+            Random random = new Random();
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            numbers = Enumerable.Range(1, 10).ToArray();// เริ่มต้น array เป็นเลข 1–10 แล้วสับ
+            Shuffle(numbers);
+
+            
+            Button[] buttons = { button2, button3, button4, button5, button6, button7, button8, button9, button10, button11 };// ให้ปุ่มทั้งหมดในเกม
+
+            
+            for (int i = 0; i < buttons.Length; i++)// ใส่เลขตามลำดับของ numbers
+            {
+                buttons[i].Text = numbers[i].ToString();
+                buttons[i].Enabled = true;
+            }
+
+            
+            tabControl1.SelectedIndex = 1;// เปลี่ยนหน้าไป tab ที่ 1 (หน้าเกม)
+
+            ScoreP1 = 0; // RESET GAME
+            ScoreP2 = 0;
+            NumberP1 = 0;
+            NumberP2 = 0;
+            foreach (var btn in buttons)
+            {
+                btn.Enabled = false;
+
+            }
+            Task.Delay(CardShowTime).ContinueWith(_ =>// ปรับดีเลย์ได้ ที่ Card Show time
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    foreach (var btn in buttons)
+                    {
+                        btn.Enabled = true;
+                        btn.Text = "?";
+                        Turn = 1; 
+                        State = 0; 
+                        
+
+                    }
+                });
+            });
+        }
+
+
+
+        private void HandleClick(Button btn) 
+        {
+            if (!btn.Enabled) return; // กดปุ่มปิดอยู่
+
+            int index = GetButtonIndex(btn); //หา index ของปุ่ม
+            //if (index < 0 || index >= numbers.Length) return;
+
+            int num = numbers[index];
+            btn.Text = num.ToString();
+            btn.Enabled = false;
+
+            if (Turn == 1 && State == 0)
+            {
+                NumberP1 = num; //ให้ค่าการ์ด ใส่ใน P1
+                Turn = 2;       //เปลี่ยนเทิน
+                State = 1;      // เปลี่ยนสเตต
+            }
+            else if (Turn == 2 && State == 1)
+            {
+                NumberP2 = num; 
+                State = 0;
+                Turn = 1;
+                CompareRound();  // เช็ค P1 P2 ว่าใครมากกว่า
+            }
+        }
+
+        private int GetButtonIndex(Button btn) // ปุ่ม index 
+        {
+            if (btn == button2) return 0; 
+            if (btn == button3) return 1;
+            if (btn == button4) return 2;
+            if (btn == button5) return 3;
+            if (btn == button6) return 4;
+            if (btn == button7) return 5;
+            if (btn == button8) return 6;
+            if (btn == button9) return 7;
+            if (btn == button10) return 8;
+            if (btn == button11) return 9;
+            return -9;
+        }
+
+        private void CompareRound()
+        {
+            if (NumberP1 > NumberP2)
+                ScoreP1++;
+            else if (NumberP2 > NumberP1)
+                ScoreP2++;
+
+            TXTScoreP1.Text = "Score P1 >> " + ScoreP1.ToString();
+            TXTScoreP2.Text = "Score P2 >> " + ScoreP2.ToString();
+        }
+
+        // ปุ่มทั้งหมดใช้ HandleClick()
+        private void button2_Click(object sender, EventArgs e) => HandleClick(button2);
+        private void button3_Click(object sender, EventArgs e) => HandleClick(button3);
+        private void button4_Click(object sender, EventArgs e) => HandleClick(button4);
+        private void button5_Click(object sender, EventArgs e) => HandleClick(button5);
+        private void button6_Click(object sender, EventArgs e) => HandleClick(button6);
+        private void button7_Click(object sender, EventArgs e) => HandleClick(button7);
+        private void button8_Click(object sender, EventArgs e) => HandleClick(button8);
+        private void button9_Click(object sender, EventArgs e) => HandleClick(button9);
+        private void button10_Click(object sender, EventArgs e) => HandleClick(button10);
+        private void button11_Click(object sender, EventArgs e) => HandleClick(button11);
+
+        private void Debug_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DebugNumber2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
